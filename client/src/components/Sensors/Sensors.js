8265f9c7c5d90b0df51e49bed2d5a0029670054a
nchernyshov <xpom55@mail.ru>
2021-05-19 22:21:46 +0300

    feat: connected ws and stored in state map

--- client/src/components/Sensors/Sensors.js
+++ client/src/components/Sensors/Sensors.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { styled } from 'linaria/react';
+
 import { useSensorsWSContext } from '../../context/SensorsWS/SensorsWSContext';
 
 export const Sensors = () => {
--- client/src/context/SensorsWS/SensorsWSProvider.js
+++ client/src/context/SensorsWS/SensorsWSProvider.js
@@ -1,9 +1,44 @@
-import React from 'react';
+import React, { useMemo, useEffect, useCallback, useState } from 'react';
+import { w3cwebsocket } from 'websocket';
 
 import { SensorsWSContext } from './SensorsWSContext';
 
+const client = new w3cwebsocket('ws://127.0.0.1:5000');
+
 export const SensorsWSProvider = ({ children }) => {
-  const value = { sensorsMap: {} };
+  const [sensorsMap, setSensorsMap] = useState({});
+
+  const updateSensorsMap = useCallback((data) => {
+    const { id, name, connected, unit, value } = data;
+
+    setSensorsMap((innerState) => ({
+      ...innerState,
+      [id]: {
+        name,
+        connected,
+        unit,
+        value,
+      },
+    }));
+  }, []);
+
+  useEffect(() => {
+    client.onopen = () => {
+      console.warn('WebSocket Client Connected');
+    };
+
+    client.onmessage = (message) => {
+      const data = JSON.parse(message.data);
+      console.warn(message, data);
+
+      if (data) {
+        // If data exists - update sensorsMap
+        updateSensorsMap(data);
+      }
+    };
+  }, []);
+
+  const value = useMemo(() => ({ sensorsMap }), [sensorsMap]);
 
   return (
     <SensorsWSContext.Provider value={value}>
